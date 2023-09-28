a5fca36997f314c6e9c51cfc82165e8171b73cfa
svetlana_klimova <sfettkoo@mail.ru>
2023-09-28 17:20:55 +0300

    done

---
+++ Java8/src/main/java/Main.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String file = args[0];
+        String operation = args[1];
+        String text ="";
+        if (args.length > 2) {
+             text = args[2];
+        }
+
+        switch (operation) {
+            case "-create" -> create(file);
+            case "-write" -> write(file, text);
+            case "-delete" -> delete(file);
+            case "-read" -> read(file);
+        }
+    }
+
+    public static void read(String path) throws IOException{ // /Users/svetlanaklimova/Desktop/Week_3/Java8/src/main/java/text.txt -read
+        FileReader reader = new FileReader(path);
+        while (reader.read() != -1) {
+            System.out.print((char)reader.read());
+        }
+        reader.close();
+    }
+
+    public static void write(String path , String text) throws IOException { // /Users/svetlanaklimova/Desktop/Week_3/Java8/src/main/java/text.txt -write test
+        FileWriter writer = new FileWriter(path);
+        writer.write(text);
+        writer.flush();
+        writer.close();
+    }
+
+    public static void delete(String path){ // /Users/svetlanaklimova/Desktop/Week_3/Java8/src/main/java/text.txt -delete
+        File file = new File(path);
+        file.delete();
+    }
+
+    public static void create(String path) throws IOException { ///Users/svetlanaklimova/Desktop/Week_3/Java8/src/main/java/ -create
+        File path1 = new File(path);
+        if (path1.isDirectory()){
+            Files.createFile(Path.of(path + "test.txt"));
+        }
+    }
+
+}
